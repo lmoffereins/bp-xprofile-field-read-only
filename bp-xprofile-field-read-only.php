e62923c1089138cee94a8b7d43612d39c0022f0a
Laurens Offereins <lmoffereins@gmail.com>
2018-04-22 19:24:12 +0200

    Update to version 1.2.1

--- bp-xprofile-field-read-only.php
+++ bp-xprofile-field-read-only.php
@@ -11,7 +11,7 @@
  * Plugin Name:       BP XProfile Field Read Only
  * Description:       Make BuddyPress XProfile fields uneditable or hidden for non-admins
  * Plugin URI:        https://github.com/lmoffereins/bp-xprofile-field-read-only/
- * Version:           1.2.0
+ * Version:           1.2.1
  * Author:            Laurens Offereins
  * Author URI:        https://github.com/lmoffereins/
  * Text Domain:       bp-xprofile-field-read-only
@@ -77,7 +77,7 @@ final class BP_XProfile_Field_Read_Only {
 
 		/** Versions **********************************************************/
 		
-		$this->version      = '1.2.0';
+		$this->version      = '1.2.1';
 		
 		/** Paths *************************************************************/
 		
--- readme.txt
+++ readme.txt
@@ -3,7 +3,7 @@ Contributors: Offereins
 Tags: buddypress, xprofile, field, read, admin, only, readonly, disable, disabled, hide
 Requires at least: 4.0, BP 2.2
 Tested up to: 4.9, BP 3.0
-Stable tag: 1.2.0
+Stable tag: 1.2.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
